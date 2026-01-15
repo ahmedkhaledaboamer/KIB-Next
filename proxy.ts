b1d6ee02106256150bce19d8f58a4f63db9377dc
AhmedMedhat <75226176+AhmedMedhat77@users.noreply.github.com>
2026-01-15 21:50:55 +0200

    Migrate From Middle ware to proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,12 @@
 import createMiddleware from 'next-intl/middleware';
+import { NextRequest } from 'next/server';
 import { routing } from './i18n/routing';
 
-export default createMiddleware(routing);
+const intlMiddleware = createMiddleware(routing);
+
+export function proxy(request: NextRequest) {
+  return intlMiddleware(request as NextRequest);
+}
 
 export const config = {
   matcher: [
